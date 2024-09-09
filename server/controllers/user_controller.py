7571b250c4ccfdc28afbc245a8ae020c8b04a1de
yaokiet <ykleong.2022@scis.smu.edu.sg>
2024-09-09 14:58:52 +0800

    reverty

--- server/controllers/user_controller.py
+++ server/controllers/user_controller.py
@@ -1,5 +1,4 @@
-
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, abort
 from extensions import db
 from models.user import User
 
@@ -10,9 +9,19 @@ def get_users():
     users = User.query.all()
     return jsonify([user.serialize() for user in users])
 
+# Fetch user by their ID
+@user_bp.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        return abort(404, description="User not found")
+    return jsonify(user.serialize())
+
 @user_bp.route('/users', methods=['POST'])
 def create_user():
     data = request.json
+    if 'username' not in data or 'email' not in data:
+        return jsonify({'error': 'Invalid input, username and email are required'}), 400
     new_user = User(username=data['username'], email=data['email'])
     db.session.add(new_user)
     db.session.commit()
--- server/models/user.py
+++ server/models/user.py
@@ -7,6 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    role = db.Column(db.Integer, unique=False, nullable = False)
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -15,5 +16,7 @@ class User(db.Model):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email, 
+            'role': self.role, 
+
         }
