00f49a04361a2595a1e78af71c9d1e80c451ee0f
yaokiet <ykleong.2022@scis.smu.edu.sg>
2024-09-17 22:42:46 +0800

    user_login

--- server/app.py
+++ server/app.py
@@ -1,11 +1,15 @@
 
 from flask import Flask
+from flask_cors import CORS
 from extensions import db
 from flask_migrate import Migrate
 from controllers.user_controller import user_bp
 
+
 def create_app():
     app = Flask(__name__)
+    CORS(app)
+
 
     # Set configurations
     app.config.from_object('config.Config')
