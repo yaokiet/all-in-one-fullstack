34cab6b7f2cb2038e30d326b19c779584c0ae532
Y4njie <yanjie.hong.2022@scis.smu.edu.sg>
2024-09-19 16:47:42 +0800

    create page.js for own schedule

---
+++ client/src/app/ownschedule/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export default function OwnSchedule() {
+    // const ...
+
+    //useEffect ... 
+
+
+
+    return (
+        <div>
+            <h1>Own Schedule</h1>
+            {/* ... */}
+        </div>
+    )
+}
