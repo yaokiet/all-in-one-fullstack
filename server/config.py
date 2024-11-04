779995affaa458c3a4be33d109c712b41795049f
JoelSngZhenWei <joelsngzw@gmail.com>
2024-11-04 18:26:50 +0800

    pausing testing because it deletes everything

--- server/config.py
+++ server/config.py
@@ -23,7 +23,7 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     """Testing config."""
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # In-memory database for testing
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test_database.db'  # In-memory database for testing
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # In-memory database for testing
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///test_database.db'  # In-memory database for testing
     TESTING = True
     DEBUG = True
