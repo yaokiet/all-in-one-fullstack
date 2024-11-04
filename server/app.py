4e86aafa5d3ca76cdc872c24042a93c8ecc6e63d
JoelSngZhenWei <joelsngzw@gmail.com>
2024-11-04 22:58:57 +0800

    changes for deploy

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from extensions import db
 from flask_migrate import Migrate
@@ -8,8 +8,12 @@ from controllers.employee_controller import employee_bp
 from controllers.team_view_controller import team_view_bp
 from controllers.apply_controller import apply_bp
 from controllers.manager_controller import manager_bp
-# from utils.scheduler import start_scheduler
 from flask_session import Session
+import git
+import hmac
+import hashlib
+import os
+from dotenv import load_dotenv
 
 # migrate = Migrate()
 sess = Session()
@@ -53,6 +57,33 @@ def create_app(config_class=DevelopmentConfig):
         response.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
         response.headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS"
         return response
+
+    # Webhook route
+    @app.route('/update_server', methods=['POST'])
+    def update_server():
+        x_hub_signature = request.headers.get('X-Hub-Signature')
+        w_secret = os.getenv("WEBHOOK_SECRET", "your_default_secret")
+
+        # Verify the signature
+        if not is_valid_signature(x_hub_signature, request.data, w_secret):
+            return 'Invalid signature', 403
+
+        # Pull the latest changes from the repo
+        repo = git.Repo('/home/jszw00/all-in-one-fullstack')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+
+    def is_valid_signature(x_hub_signature, data, private_key):
+        if not x_hub_signature:
+            return False
+        hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+        algorithm = hashlib.__dict__.get(hash_algorithm)
+        if algorithm is None:
+            return False
+        encoded_key = bytes(private_key, 'latin-1')
+        mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+        return hmac.compare_digest(mac.hexdigest(), github_signature)
 
 
 
