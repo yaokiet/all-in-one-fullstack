c7ba1718e1913a5054fd0bfdf74c829f8159781e
JoelSngZhenWei <joelsngzw@gmail.com>
2024-11-04 23:18:07 +0800

    changes for deploy

--- server/app.py
+++ server/app.py
@@ -61,11 +61,11 @@ def create_app(config_class=DevelopmentConfig):
     # Webhook route
     @app.route('/update_server', methods=['POST'])
     def update_server():
-        x_hub_signature = request.headers.get('X-Hub-Signature')
+        x_hub_signature = request.headers.get('X-Hub-Signature-256')
         w_secret = os.getenv("WEBHOOK_SECRET", "tieguanyin")
 
         # Verify the signature
-        if not is_valid_signature(x_hub_signature, request.data, w_secret):
+        if not is_valid_signature(request.data, w_secret, x_hub_signature):
             return 'Invalid signature', 403
 
         # Pull the latest changes from the repo
@@ -74,18 +74,12 @@ def create_app(config_class=DevelopmentConfig):
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
 
-    def is_valid_signature(x_hub_signature, data, private_key):
-        if not x_hub_signature:
+    def is_valid_signature(payload_body, secret_token, signature_header):
+        if not signature_header:
             return False
-        hash_algorithm, github_signature = x_hub_signature.split('=', 1)
-        algorithm = hashlib.__dict__.get(hash_algorithm)
-        if algorithm is None:
-            return False
-        encoded_key = bytes(private_key, 'latin-1')
-        mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
-        print("Generated Signature:", mac.hexdigest())
-        print("GitHub Signature:", github_signature)
-        return hmac.compare_digest(mac.hexdigest(), github_signature)
+        hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+        expected_signature = "sha256=" + hash_object.hexdigest()
+        return hmac.compare_digest(expected_signature, signature_header)
 
 
 
