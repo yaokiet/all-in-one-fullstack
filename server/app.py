d9e7c2426cd1f2cb67c1c753001fc41e1aa70926
JoelSngZhenWei <joelsngzw@gmail.com>
2024-11-04 20:38:24 +0800

    tests are fixed

--- server/app.py
+++ server/app.py
@@ -16,7 +16,6 @@ sess = Session()
 
 def create_app(config_class = DevelopmentConfig):
     app = Flask(__name__)
-    # CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
     CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
 
     # Set configurations
