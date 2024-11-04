b0dcc1390cdb7d44b74de3129369aa6a9c394938
JoelSngZhenWei <joelsngzw@gmail.com>
2024-11-04 23:08:56 +0800

    changes for deploy

--- server/app.py
+++ server/app.py
@@ -62,7 +62,7 @@ def create_app(config_class=DevelopmentConfig):
     @app.route('/update_server', methods=['POST'])
     def update_server():
         x_hub_signature = request.headers.get('X-Hub-Signature')
-        w_secret = os.getenv("WEBHOOK_SECRET", "your_default_secret")
+        w_secret = os.getenv("WEBHOOK_SECRET", "tieguanyin")
 
         # Verify the signature
         if not is_valid_signature(x_hub_signature, request.data, w_secret):
@@ -83,6 +83,8 @@ def create_app(config_class=DevelopmentConfig):
             return False
         encoded_key = bytes(private_key, 'latin-1')
         mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+        print("Generated Signature:", mac.hexdigest())
+        print("GitHub Signature:", github_signature)
         return hmac.compare_digest(mac.hexdigest(), github_signature)
 
 
